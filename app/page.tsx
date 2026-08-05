236c74de7844c803e7ed1ad468fb2e354e0f22df
abrarmehmood423-code <abrarmehmood423@gmail.com>
2026-08-05 20:16:23 +0100

    Enable cloud sync bridge on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import LifeOS from "@/components/life-os";
 import ModuleLauncher from "@/components/module-launcher";
 import AccountShortcut from "@/components/account-shortcut";
+import CloudSyncBridge from "@/components/cloud-sync-bridge";
 
 export default function HomePage() {
   return (
     <>
+      <CloudSyncBridge />
       <LifeOS />
       <AccountShortcut />
       <ModuleLauncher />
