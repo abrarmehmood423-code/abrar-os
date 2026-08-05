7385775a55f4d48cb79b9110f6eb5820431e5749
abrarmehmood423-code <abrarmehmood423@gmail.com>
2026-08-05 19:23:55 +0100

    Show cloud account link on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import LifeOS from "@/components/life-os";
 import ModuleLauncher from "@/components/module-launcher";
+import AccountShortcut from "@/components/account-shortcut";
 
 export default function HomePage() {
   return (
     <>
       <LifeOS />
+      <AccountShortcut />
       <ModuleLauncher />
     </>
   );
